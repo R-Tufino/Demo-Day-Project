a0313f7fe1e5649482b87d1fae139267b0cd0d90
Richie Tufino <richie.tufino@gmail.com>
2021-08-18 16:05:52 -0400

    Second Commit, added search bar and show more button

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 const url = 'https://data.cityofnewyork.us/resource/kvhd-5fmu.json'
 let data;
 
-
-
-let Box = document.createElement("div");
-Box.className = "Card"
-
 fetch(url)
   .then(function(response) {
     console.log("This is the response: ", response);
@@ -16,54 +11,103 @@ fetch(url)
     console.log("This is the JSON", myJson);
 
     for (let i = 0; i<data.length; i++) {
-    let card = document.createElement("div");
-    card.className = "card"
+      let Box = document.createElement("div");
+      Box.className = "Card"
     
+  //Program Name
     const ProName = data[i].program_name;
     const Acro = data[i].program_acronym;
-    const paragraph1 = document.createElement("p");
+    const paragraph1 = document.createElement("h1");
     paragraph1.innerText = ProName + " (" + Acro + ")";
+//Targets Div
+    const Target = document.createElement("div");
+    Target.id = "Dog"
     
-
-    const Type = data[i].page_type;
-    const paragraph2 = document.createElement("p");
-    paragraph2.innerText = "Type of Information: " + Type;
-    
-    
-    const Cat = data[i].program_category;
-    const paragraph3 = document.createElement("p");
-    paragraph3.innerText = "Type of Program: " + Cat;
-    
-
     const Serve = data[i].population_served;
-    const paragraph4 = document.createElement("p");
-    paragraph4.innerText = "Target Population: " + Serve;
-    
+    const paragraph2 = document.createElement("p");
+    paragraph2.classList.toggle("class1");
+    paragraph2.innerText = "Target Population: " + Serve;
 
     const Age = data[i].age_group;
-    const paragraph5 = document.createElement("p");
-    paragraph5.innerText = "Target Age Group: " + Age;
-    
-    
-    const Excerpt = data[i].brief_excerpt;
-    const paragraph6 = document.createElement("p");
-    paragraph6.innerText = Excerpt;
-    
-
+    const paragraph3 = document.createElement("p");
+    paragraph3.classList.toggle("class1");
+    paragraph3.innerText = "Target Age Group: " + Age;
+//
+  //Program Description
+    const Deep = data[i].program_description;
+    const paragraph4 = document.createElement("p");
+    paragraph4.innerHTML = Deep;
+////////////////////////////////////////////////////////////////////////////////////////////
+  //Eligibility Requirments
     const Eligible = data[i].plain_language_eligibility;
+    const paragraph5 = document.createElement("p");
+    paragraph5.classList.toggle("more");
+    paragraph5.innerHTML = "Eligibility: " + Eligible;
+  //Warning
+    const Warning = data[i].heads_up;
+    const paragraph6 = document.createElement("p");
+    paragraph6.classList.toggle("more");
+    paragraph6.innerHTML = "FAQs: " + Warning;
+  //Different Application Options
+    const Apply = data[i].how_to_apply_summary;
     const paragraph7 = document.createElement("p");
-    paragraph7.innerText = Eligible;
+    paragraph7.classList.toggle("more");
+    paragraph7.innerHTML = "How to Apply: " + Apply;
+
+  //List of Help Options
+    const Help = data[i].get_help_summary;
+    const paragraph8 = document.createElement("p");
+    paragraph8.classList.toggle("more");
+    paragraph8.innerHTML = "Help: " + Help;
+
+//More or Less Button
+    var More = document.createElement("button");
+    More.id = "myBtn"
+    More.innerText = "More Info";
+    More.addEventListener("click", Fun);
+    function Fun() {
+      var btnText = document.getElementById("myBtn");
+      paragraph8.style.display = "none";
+
+      if (btnText.innerHTML === "Less Info") {
+        btnText.innerHTML = "More Info"; 
+        Box.removeChild(paragraph5);
+        Box.removeChild(paragraph6);
+        Box.removeChild(paragraph7);
+        Box.removeChild(paragraph8);
+      } 
+      else {
+        btnText.innerHTML = "Less Info"; 
+        Box.appendChild(paragraph5);
+        Box.appendChild(paragraph6);
+        Box.appendChild(paragraph7);
+        Box.appendChild(paragraph8);
+      }
+    }
+///////////////////////////////////////////////////
 
     Box.appendChild(paragraph1);
-    Box.appendChild(paragraph2);
-    Box.appendChild(paragraph3);
+    Box.appendChild(Target);
+      Target.appendChild(paragraph2);
+      Target.appendChild(paragraph3);
     Box.appendChild(paragraph4);
-    Box.appendChild(paragraph5);
-    Box.appendChild(paragraph6);
-    Box.appendChild(paragraph7);
+    Box.appendChild(More);
     document.body.appendChild(Box);
-    
     }
-    
   })
+//Search Bar
+  function searchPage() {
+    let input = document.getElementById('searchBar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('Card');
 
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";
+        }
+    }
+
+}
